f0670190a12763197792856b631ad312345713f7
AhmadAlzaza <ahmad.alzaza00@gmail.com>
2026-07-17 03:40:46 +0300

    feat: cache categories index with write invalidation

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,12 +6,19 @@ use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryController extends Controller
 {
+    private const CACHE_KEY = 'categories.all';
+
+    private const CACHE_TTL = 3600;
+
     public function index()
     {
-        $categories = Category::paginate(15);
+        $categories = Cache::remember(self::CACHE_KEY, self::CACHE_TTL, function () {
+            return Category::orderBy('name')->get();
+        });
 
         return CategoryResource::collection($categories);
     }
@@ -19,6 +26,8 @@ class CategoryController extends Controller
     public function store(StoreCategoryRequest $request)
     {
         $category = Category::create($request->validated());
+
+        Cache::forget(self::CACHE_KEY);
 
         return (new CategoryResource($category))->response()->setStatusCode(201);
     }
@@ -32,12 +41,16 @@ class CategoryController extends Controller
     {
         $category->update($request->validated());
 
+        Cache::forget(self::CACHE_KEY);
+
         return new CategoryResource($category);
     }
 
     public function destroy(Category $category)
     {
         $category->delete();
+
+        Cache::forget(self::CACHE_KEY);
 
         return response()->json(['message' => 'Category deleted successfully']);
     }
