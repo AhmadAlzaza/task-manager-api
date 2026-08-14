095052849e39b1987f9b4d10209980fe107a8ab6
AhmadAlzaza <ahmad.alzaza00@gmail.com>
2026-08-15 02:29:27 +0300

    feat(api): add v1 prefix to API routes

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,19 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register'])
-    ->middleware('throttle:5,1');
-Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('tasks', TaskController::class);
+Route::prefix('v1')->group(function () {
 
-    Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
+    Route::post('/register', [AuthController::class, 'register'])
+        ->middleware('throttle:5,1');
+    Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::apiResource('tasks', TaskController::class);
 
-    Route::middleware('can:manage-categories')->group(function () {
-        Route::apiResource('categories', CategoryController::class)->only(['store', 'update', 'destroy']);
+        Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
+
+        Route::middleware('can:manage-categories')->group(function () {
+            Route::apiResource('categories', CategoryController::class)->only(['store', 'update', 'destroy']);
+        });
     });
 });
